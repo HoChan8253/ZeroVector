76b48534730930beae44f5ee21895c369d4e33e2
HoChan8253 <nahcohochan@gmail.com>
2026-02-24 13:07:05 +0900

    [Feat] Bullet spread logic
    사격 시 탄퍼짐 적용

--- Assets/Scripts/Player/Shoot/GunController.cs
+++ Assets/Scripts/Player/Shoot/GunController.cs
@@ -12,13 +12,18 @@ public class GunController : MonoBehaviour
     public float _range = 100f;
     public float _damage = 25f;
 
+    [Header("Spread")]
+    public float _spreadAmount = 0.02f;   // 기본 퍼짐
+    public float _maxSpread = 0.08f;      // 최대 퍼짐
+    private float _currentSpread;
+
     private float _nextFireTime;
 
     public PlayerLook _look;
     public float _recoilPitchAmount = 2f;
     public float _recoilYawAmount = 1f;
 
-    void Awake()
+    private void Awake()
     {
         if (_input == null)
             _input = GetComponentInParent<PlayerInputHub>();
@@ -27,7 +32,7 @@ public class GunController : MonoBehaviour
             _look = GetComponentInParent<PlayerLook>();
     }
 
-    void Update()
+    private void Update()
     {
         // 연사 조건
         if (_input.FireHeld && Time.time >= _nextFireTime)
@@ -42,14 +47,23 @@ public class GunController : MonoBehaviour
         }
     }
 
-    void Fire()
+    private void Fire()
     {
         _anim.SetTrigger("Shoot");
 
         float randomYaw = Random.Range(-_recoilYawAmount, _recoilYawAmount);
         _look.AddRecoil(_recoilPitchAmount, randomYaw);
 
-        Ray ray = new Ray(_cam.transform.position, _cam.transform.forward);
+        _currentSpread += _spreadAmount;
+        _currentSpread = Mathf.Clamp(_currentSpread, 0f, _maxSpread);
+
+        Vector3 direction = _cam.transform.forward;
+        // 랜덤 탄퍼짐 추가
+        direction += _cam.transform.right * Random.Range(-_currentSpread, _currentSpread);
+        direction += _cam.transform.up * Random.Range(-_currentSpread, _currentSpread);
+        direction.Normalize();
+
+        Ray ray = new Ray(_cam.transform.position, direction);
 
         if (Physics.Raycast(ray, out RaycastHit hit, _range))
         {
