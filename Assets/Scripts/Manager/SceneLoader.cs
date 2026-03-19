c614620c54eeb5172ee9ad30ee57a25dc39c4751
HoChan8253 <nahcohochan@gmail.com>
2026-03-20 03:24:39 +0900

    [Feat] Title scene and main menu
    타이틀 씬, 메인 메뉴, 옵션 구현

---
+++ Assets/Scripts/Manager/SceneLoader.cs
@@ -0,0 +1,58 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance { get; private set; }
+
+    [Header("로딩 UI")]
+    [SerializeField] private GameObject _loadingPanel;
+    [SerializeField] private Slider _progressBar;
+    [SerializeField] private TextMeshProUGUI _progressText;
+
+    private void Awake()
+    {
+        if (Instance != null) { Destroy(gameObject); return; }
+        Instance = this;
+        _loadingPanel?.SetActive(false);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(CoLoad(sceneName));
+    }
+
+    private IEnumerator CoLoad(string sceneName)
+    {
+        _loadingPanel?.SetActive(true);
+
+        yield return null; // 한 프레임 대기
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+        async.allowSceneActivation = false;
+
+        while (!async.isDone)
+        {
+            // 0 ~ 0.9 범위를 0 ~ 1로 변환
+            float progress = Mathf.Clamp01(async.progress / 0.9f);
+
+            if (_progressBar) _progressBar.value = progress;
+            if (_progressText) _progressText.text = $"{(int)(progress * 100f)}%";
+
+            // 로드 완료
+            if (async.progress >= 0.9f)
+            {
+                if (_progressText) _progressText.text = "100%";
+                if (_progressBar) _progressBar.value = 1f;
+
+                yield return new WaitForSeconds(0.5f); // 잠깐 대기 후 전환
+                async.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TitleMenuUI.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleMenuUI : MonoBehaviour
+{
+    [Header("버튼")]
+    [SerializeField] private Button _gameStartBtn;
+    [SerializeField] private Button _howToPlayBtn;
+    [SerializeField] private Button _optionsBtn;
+    [SerializeField] private Button _quitBtn;
+
+    [Header("패널")]
+    [SerializeField] private GameObject _howToPlayPanel;
+    [SerializeField] private Button _howToPlayCloseBtn;
+    [SerializeField] private OptionsUI _optionsUI;
+
+    [Header("씬 이름")]
+    [SerializeField] private string _gameSceneName = "InGame";
+
+    private void Awake()
+    {
+        _gameStartBtn?.onClick.AddListener(OnGameStart);
+        _howToPlayBtn?.onClick.AddListener(OnHowToPlay);
+        _optionsBtn?.onClick.AddListener(OnOptions);
+        _quitBtn?.onClick.AddListener(OnQuit);
+        _howToPlayCloseBtn?.onClick.AddListener(() => _howToPlayPanel?.SetActive(false));
+
+        _howToPlayPanel?.SetActive(false);
+    }
+
+    private void OnGameStart()
+    {
+        SceneLoader.Instance?.LoadScene(_gameSceneName);
+    }
+
+    private void OnHowToPlay()
+    {
+        _howToPlayPanel?.SetActive(true);
+    }
+
+    private void OnOptions()
+    {
+        _optionsUI?.Open();
+    }
+
+    private void OnQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
